cac7307893e4ef7eef3bf1d2ee44bafaedc38807
cys-solom <islam472542@beng.bu.edu.eg>
2025-04-21 02:18:35 +0200

    Update OrderForm.tsx

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -1,7 +1,14 @@
-
 import OrderFormModal from './OrderFormModal';
 import { CartItem } from '@/pages/CategoryPage';
 
+/**
+ * Props for OrderForm component
+ * @interface OrderFormProps
+ * @property {CartItem[]} cart - Array of items in the cart
+ * @property {number} total - Total amount of the order
+ * @property {() => void} onCancel - Callback function when order is canceled
+ * @property {() => void} onComplete - Callback function when order is completed
+ */
 interface OrderFormProps {
   cart: CartItem[];
   total: number;
@@ -9,9 +16,17 @@ interface OrderFormProps {
   onComplete: () => void;
 }
 
-// This component now serves as a wrapper for OrderFormModal
-// to maintain backward compatibility
-const OrderForm = (props: OrderFormProps) => {
+/**
+ * OrderForm component (wrapper for OrderFormModal)
+ * 
+ * This component serves as a backward-compatible wrapper for OrderFormModal.
+ * It passes all props directly to OrderFormModal component.
+ * 
+ * @component
+ * @param {OrderFormProps} props - Component props
+ * @returns {JSX.Element} Rendered OrderFormModal component
+ */
+const OrderForm = (props: OrderFormProps): JSX.Element => {
   return <OrderFormModal {...props} />;
 };
 
